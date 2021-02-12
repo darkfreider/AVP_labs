77782484465006edf180d88c0d1099573f0a7289
Maxim Sednev <darkfreider@gmail.com>
2021-02-12 14:14:49 +0300

    regractoring and algorithm improvement

--- matmul.cpp
+++ matmul.cpp
@@ -3,19 +3,16 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdint.h>
-#include <x86intrin.h>
+#include <math.h>
+#include <windows.h>
+//#include <x86intrin.h>
+#include <intrin.h>
 
-const int M = 32 * 32;
-const int N = 32 * 32;
-const int L = 32 * 32;
+const int M = 2 * 1024;
+const int N = 2 * 1024;
+const int L = 2 * 1024;
 
-const int blockSize = 16;
-
-double mat0[M][N];
-double mat1[N][L];
-
-double out[M][L];
-double out_block[M][L];
+const int blockSize = 8;
 
 uint64_t out_time;
 uint64_t out_block_time;
@@ -23,22 +20,41 @@ uint64_t out_block_time;
 
 void mat_mul(void)
 {
-    uint64_t start = __rdtsc();
-
-    for (int i = 0; i < M; i++)
+    double *mat0 = (double *)malloc(sizeof(double) * M * N);
+    double *mat1 = (double *)malloc(sizeof(double) * N * L);
+	double *mat_out = (double *)malloc(sizeof(double) * M * L);
+    
+	for (int i = 0; i < (M * N); i++)
     {
-        for (int j = 0; j < L; j++)
+        mat0[i] = (double)(i + 1);
+    }
+    for (int i = 0; i < (N * L); i++)
+    {
+        mat1[i] = (double)(i + 1);
+    }
+	for (int i = 0; i < (M * L); i++)
+	{
+		mat_out[i] = 0.0;
+	}
+
+	LARGE_INTEGER start;
+	QueryPerformanceCounter(&start);
+    
+	for (int i = 0; i < M; i++)
+    {
+		for (int j = 0; j < L; j++)
         {
-            for (int k = 0; k < N; k++)
+			for (int k = 0; k < N; k++)
             {
-                out[i][j] += mat0[i][k] * mat1[k][j];
+				mat_out[i * L + j] += mat0[i * N + k] * mat1[k * L + j];
             }
         }
     }
 
-    uint64_t end = __rdtsc();
-    out_time = end - start;
-    printf("%ld\n", end - start);
+	LARGE_INTEGER end;
+	QueryPerformanceCounter(&end);
+
+    out_time = end.QuadPart - start.QuadPart;
 }
 
 void mat_mul_block(void)
@@ -47,22 +63,40 @@ void mat_mul_block(void)
     assert(N % blockSize == 0);
     assert(L % blockSize == 0);
 
-    uint64_t start = __rdtsc();
+	double *mat0 = (double *)malloc(sizeof(double) * M * N);
+	double *mat1 = (double *)malloc(sizeof(double) * N * L);
+	double *mat_out = (double *)malloc(sizeof(double) * M * L);
+
+	for (int i = 0; i < (M * N); i++)
+	{
+		mat0[i] = (double)(i + 1);
+	}
+	for (int i = 0; i < (N * L); i++)
+	{
+		mat1[i] = (double)(i + 1);
+	}
+	for (int i = 0; i < (M * L); i++)
+	{
+		mat_out[i] = 0.0;
+	}
+
+	LARGE_INTEGER start;
+	QueryPerformanceCounter(&start);
 
     for (int ii = 0; ii < (M / blockSize); ii += 1)
     {
-        for (int jj = 0; jj < (L / blockSize); jj += 1)
+		for (int kk = 0; kk < (N / blockSize); kk += 1)
         {
-            for (int kk = 0; kk < (N / blockSize); kk += 1)
+			for (int jj = 0; jj < (L / blockSize); jj += 1)
             {
 
                 for (int i = ii * blockSize; i < (ii + 1) * blockSize; i++)
                 {
-                    for (int j = jj * blockSize; j < (jj + 1) * blockSize; j++)
+					for (int k = kk * blockSize; k < (kk + 1) * blockSize; k++)
                     {
-                        for (int k = kk * blockSize; k < (kk + 1) * blockSize; k++)
+						for (int j = jj * blockSize; j < (jj + 1) * blockSize; j++)
                         {
-                            out_block[i][j] += mat0[i][k] * mat1[k][j];
+							mat_out[i * L + j] += mat0[i * N + k] * mat1[k * L + j];
                         }
                     }
                 }
@@ -71,49 +105,37 @@ void mat_mul_block(void)
         }
     }
 
-    uint64_t end = __rdtsc();
-    out_block_time = end - start;
+	LARGE_INTEGER end;
+	QueryPerformanceCounter(&end);
+
+	out_block_time = end.QuadPart - start.QuadPart;
 }
 
 
 int main(void)
 {
-    double *tmp = &mat0[0][0];
-    for (int i = 0; i < (M * N); i++)
-    {
-        *tmp++ = (double)(i + 1);
-    }
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
 
-    tmp = &mat1[0][0];
-    for (int i = 0; i < (N * L); i++)
-    {
-        *tmp++ = (double)(i + 1);
-    }
-
-    mat_mul();
+    //mat_mul();
     mat_mul_block();
 
-    for (int i = 0; i < M; i++)
-    {
-        for (int j = 0; j < L; j++)
-        {
-            assert(out[i][j] == out_block[i][j]);
-        }
-    }
+    //for (int i = 0; i < M; i++)
+    //{
+    //    for (int j = 0; j < L; j++)
+    //    {
+    //        //assert(out[i][j] == out_block[i][j]);
+    //        if (out[i][j] != out_block[i][j])
+    //        {
+    //            printf("%lf\n", fabs(out[i][j] - out_block[i][j]));
+    //        }
+    //    }
+    //}
 
-    printf("simple: %ld\n", out_time);
-    printf("block: %ld\n", out_block_time);
-    printf("%lf\n", (double)out_time / (double)out_block_time);
+    //printf("simple: %lf\n", (double)out_time / (double)frequency.QuadPart);
+    printf("block: %lf\n", (double)out_block_time / (double)frequency.QuadPart);
+    //printf("%lf\n", (double)out_time / (double)out_block_time);
 
-
-    /*for (int i = 0; i < (M * L); i++)
-    {
-        if (out[i] != out_block[i])
-        {
-            printf("%lf : %lf\n", out[i], out_block[i]);
-        }
-        assert(out[i] == out_block[i]);
-    }*/
 
     return (0);
 }
